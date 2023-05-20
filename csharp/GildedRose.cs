d0239fd2ff985ebb234b2b145a4c632a6135ff82
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2023-05-20 03:56:30 +0300

    Refactor bloated if blocks to switch()
    * This is an step seeking understading towards greater refactoring

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -10,62 +10,72 @@ namespace csharp
             this.Items = InventoryItem.ConvertItemsToInventoryItems(Items);
         }
 
+        private void UpdateItem(InventoryItem item)
+        {
+            switch (item.Name)
+            {
+                case SpecialCases.AGED_BRIE:
+                    {
+                        item.IncreaseQuality();
+                        item.UpdateExpiry();
+
+                        if (item.IsExpired())
+                        {
+                            item.IncreaseQuality();
+                        }
+                    }
+                    break;
+                case SpecialCases.BACKSTAGE_PASSES:
+                    {
+                        item.IncreaseQuality();
+
+                        if (item.Expiry < 11)
+                        {
+                            item.IncreaseQuality();
+                        }
+
+                        if (item.Expiry < 6)
+                        {
+                            item.IncreaseQuality();
+                        }
+
+                        item.UpdateExpiry();
+
+                        if (item.IsExpired())
+                        {
+                            item.DropQualityToZero();
+                        }
+                    }
+                    break;
+                case SpecialCases.SULFURAS:
+                    {
+                        // Nothing to be done for Sulfuras
+                    }
+                    break;
+                default:
+                    {
+                        item.DecreaseQuality();
+                        item.UpdateExpiry();
+
+                        if (item.IsExpired())
+                        {
+                            item.DecreaseQuality();
+                        }
+                    }
+                    break;
+            }
+        }
+
         public void DailyUpdate()
+        {
+            UpdateQuality();
+        }
+
+        public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != SpecialCases.AGED_BRIE &&
-                    Items[i].Name != SpecialCases.BACKSTAGE_PASSES)
-                {
-                    if (Items[i].Name != SpecialCases.SULFURAS)
-                    {
-                        Items[i].DecreaseQuality();
-                    }
-                }
-                else
-                {
-                    Items[i].IncreaseQuality();
-
-                    if (Items[i].Name == SpecialCases.BACKSTAGE_PASSES)
-                    {
-                        if (Items[i].Expiry < 11)
-                        {
-                            Items[i].IncreaseQuality();
-                        }
-
-                        if (Items[i].Expiry < 6)
-                        {
-                            Items[i].IncreaseQuality();
-                        }
-                    }
-                }
-
-                if (Items[i].Name != SpecialCases.SULFURAS)
-                {
-                    Items[i].UpdateExpiry();
-                }
-
-                if (Items[i].IsExpired())
-                {
-                    if (Items[i].Name != SpecialCases.AGED_BRIE)
-                    {
-                        if (Items[i].Name != SpecialCases.BACKSTAGE_PASSES)
-                        {
-                            if (Items[i].Name != SpecialCases.SULFURAS)
-                            {
-                                Items[i].DecreaseQuality();
-                            }
-                        }
-                        else
-                        {
-                            Items[i].DropQualityToZero();
-                        }
-                    }
-                    else
-                    {
-                        Items[i].IncreaseQuality();
-                    }
-                }
+                UpdateItem(Items[i]);
             }
         }
     }
