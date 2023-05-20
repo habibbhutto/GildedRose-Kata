698174d2f6fff6d2f5f51b41e15ed8a5a1978f99
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2023-05-20 03:04:01 +0300

    Add InventoryItem as domain Entity
    It seems Item is a contract model that
    cannot be modified. We are adding InventoryItem as
    internal domain Entity to restructure our app.

---
+++ csharp/InventoryItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class InventoryItem
+    {
+        private Item item;
+
+        public InventoryItem(Item item)
+        {
+            this.item = item;
+        }
+
+        public void increaseQuality()
+        {
+            item.Quality++;
+        }
+
+        public void decreaseQuality()
+        {
+            item.Quality--;
+        }
+
+        public void updateExpiry()
+        {
+            item.SellIn--;
+        }
+
+        public bool isExpired()
+        {
+            return item.SellIn < 0;
+        }
+
+        public static IList<InventoryItem> convertItemsToInventoryItems(IList<Item> items)
+        {
+            IList<InventoryItem> inventoryItems = new List<InventoryItem>();
+            foreach (var item in items)
+            {
+                inventoryItems.Add(new InventoryItem(item));
+            }
+
+            return inventoryItems;
+        }
+    }
+}
